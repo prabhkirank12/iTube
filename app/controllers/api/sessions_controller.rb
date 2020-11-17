2c70acb1da1ee36614e8f8b9e513bd6a8c87e888
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-17 12:11:13 -0500

    Working on user auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,22 @@
 class Api::SessionsController < ApplicationController
+   def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user 
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: ["Invalid username or password"], status: 422
+        end
+   end
+
+   def destroy
+        if current_user
+            logout!
+        else
+            render json: ["There is no user logged in"], status: 404
+        end
+   end
 end
--- frontend/i_tube.jsx
+++ frontend/i_tube.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root"
+import { signup, login, logout } from "./util/session_api_util"
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to your iTube</h1>, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={store} />, root);
+
+    //Testing
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
 });
