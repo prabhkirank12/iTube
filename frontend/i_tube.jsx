323c1bbec0a74e9a6517369b87354f0384e93eb6
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-17 19:22:49 -0500

    Added current user to preloadedState

--- frontend/i_tube.jsx
+++ frontend/i_tube.jsx
@@ -7,9 +7,26 @@ import { signup, login, logout } from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const store = configureStore();
+    // const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: {
+                    [window.currentUser.id]: window.currentUser
+                }
+            },
+            session: {
+                id: window.currentUser.id
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    
     ReactDOM.render(<Root store={store} />, root);
-
     //Testing
     window.getState = store.getState;
     window.dispatch = store.dispatch;
