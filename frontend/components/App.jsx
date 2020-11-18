1eb7b354704ff286ae2a4ef32638c8e2e0293734
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-17 19:20:22 -0500

    Added routes for login/signup form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom'
+
+import HomeContainer from './home/home_container';
+import SignUpFormContainer from './user/signup_form_container';
+import LogInFormContainer from './user/login_form_container';
 
 const App = () => (
     <div>
-        <h1>Welcome to iTube</h1>
+        <header>
+            <h1>Welcome to iTube</h1>
+            <HomeContainer />
+        </header>
+        <Switch>
+            <Route exact path="/login" component={LogInFormContainer} />
+            <Route exact path="/signup" component={SignUpFormContainer} />
+        </Switch>
     </div>
 );
 
