6bb1360b71f68e67bbdefb448e1a32496db35192
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-23 23:19:03 -0500

    Video Reducer

---
+++ frontend/reducers/videos_reducer.js
@@ -0,0 +1,27 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import {
+    RECEIVE_VIDEO,
+    RECEIVE_ALL_VIDEOS,
+    REMOVE_VIDEO
+} from '../actions/video_actions';
+
+
+const videosReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let newState = {...state};
+    switch (action.type) {
+        case RECEIVE_VIDEO:
+            // debugger;
+            newState = {[action.video.id]: action.video};
+            return newState;
+        case RECEIVE_ALL_VIDEOS:
+            return action.videos;
+        case REMOVE_VIDEO:
+            delete newState[action.videoId];
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default videosReducer;
