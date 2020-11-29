1ca8df3325d7808b6e2085d1f76d1c645a039380
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 12:25:45 -0500

    Likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,12 @@
 class Like < ApplicationRecord
-    validates :likeable_id, :likeable_type, :liker_id, :liked_value, presence: true
+    validates :likeable_id, :likeable_type, :liker_id, presence: true
     validates :liked_value, inclusion: { in: [1, -1]}
-    
+
     belongs_to :likeable, polymorphic: true
+    
     belongs_to :liker,
         primary_key: :id,
         foreign_key: :liker_id,
-        classname: :User
+        class_name: :User
 
 end
