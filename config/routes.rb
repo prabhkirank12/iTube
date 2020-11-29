d4b18761f50d4d1361f6af338b8aa569c21c358e
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 12:31:17 -0500

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
-    resources :videos, only: [:show, :index, :destroy, :create, :update]
+    resources :videos, only: [:show, :index, :destroy, :create, :update] do 
+        post :like, to: 'videos#like', as: 'like'
+        post :unlike, to: 'videos#unlike', as: 'unlike'
+        post :dislike, to: 'videos#dislike', as: 'dislike'
+        post :undislike, to: 'videos#undislike', as: 'undislike'
+        post :changelike, to: 'videos#change_like', as: 'changelike'
+    end
   end
 end
