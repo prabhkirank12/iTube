03c18202feebd5f9a73c308d3311374adc02b985
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 13:20:52 -0500

    Removed ensure attached

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ApplicationRecord
     validates :title, :uploader_id, presence: true
 
-    after_initialize :ensure_video_exist
+    # after_initialize :ensure_video_exist
 
     has_one_attached :video
     
@@ -12,9 +12,9 @@ class Video < ApplicationRecord
         foreign_key: :uploader_id,
         class_name: :User
 
-    def ensure_video_exist
-        errors[:video] << "There is no video attached" unless self.video.attached?
-    end
+    # def ensure_video_exist
+    #     errors[:video] << "There is no video attached" unless self.video.attached?
+    # end
 
     def likers
         self.likes.where(liked_value: 1).select(:liker_id).map{|ele| ele.liker_id}
