1f7a88e6ad24263f91b38a212e30cab3894bc4a9
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 12:24:27 -0500

    Likes/Dislikes for each video

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -39,6 +39,58 @@ class Api::VideosController < ApplicationController
         end
     end
 
+    def like 
+        @like = Like.new(liked_value: 1, liker_id: current_user.id, likeable_id: params[:video_id], likeable_type: "Video")
+        if @like.save
+            redirect_to api_video_url(params[:video_id])
+        else
+            render json: @like.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+    
+    def unlike 
+        @like = Like.new(liked_value: 1, liker_id: current_user.id, likeable_id: params[:video_id], likeable_type: "Video")
+        if @like.destroy
+            redirect_to api_video_url(params[:video_id])
+        else
+            render json: @like.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def dislike 
+        @like = Like.new(liked_value: -1, liker_id: current_user.id, likeable_id: params[:video_id], likeable_type: "Video")
+        if @like.save
+            redirect_to api_video_url(params[:video_id])
+        else
+            render json: @like.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+
+    def undislike 
+        @like = Like.new(liked_value: -1, liker_id: current_user.id, likeable_id: params[:video_id], likeable_type: "Video")
+        if @like.destroy
+            redirect_to api_video_url(params[:video_id])
+        else
+            render json: @like.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def change_like
+        @like = Like.find_by(liker_id: current_user.id, likeable_id: params[:video_id], likeable_type: "Video")
+        if (@like.liked_value === 1)
+            @like.liked_value = -1
+        else
+            @like.liked_value = 1
+        end
+
+        if @like.save
+            redirect_to api_video_url(params[:video_id])
+        else
+            render json: @like.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
     private
     def video_params
         params.require(:video).permit(:title, :description, :video)
