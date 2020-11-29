49b4a6ff443e9bca3d02edb330c520f5c2248193
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 12:26:44 -0500

    Added associations for likes

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,19 +1,28 @@
 class Video < ApplicationRecord
     validates :title, :uploader_id, presence: true
 
-    # validate :ensure_video_exist
+    after_initialize :ensure_video_exist
 
     has_one_attached :video
     
+    has_many :likes, as: :likeable, dependent: :destroy
 
     belongs_to :uploader,
         primary_key: :id,
         foreign_key: :uploader_id,
         class_name: :User
 
-    # def ensure_video_exist
-    #     errors[:video] << "There is no video attached" unless self.video.attached?
-    # end
+    def ensure_video_exist
+        errors[:video] << "There is no video attached" unless self.video.attached?
+    end
+
+    def likers
+        self.likes.where(liked_value: 1).select(:liker_id).map{|ele| ele.liker_id}
+    end
+
+    def dislikers
+        self.likes.where(liked_value: -1).select(:liker_id).map{|ele| ele.liker_id}  
+    end
     
 
 end
