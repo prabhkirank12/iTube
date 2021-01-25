cd3ae3629d9e2b19482cca9846a4533e2edbb4e0
prabhkirank12 <prabhkirank2@gmail.com>
2021-01-25 11:02:25 -0500

    Styled the search bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -49,8 +49,8 @@ class NavBar extends React.Component{
         if(!this.props.currentUser){
             return(
                 <>
-                    <Link className="home" to="/login"> <BsIcons.BsFillPersonFill className="humanIcon" /> Sign in</Link>;
-                    <button className="home" onClick={this.handleDemoSubmit}>Demo User</button>;
+                    <Link className="home" to="/login"> <BsIcons.BsFillPersonFill className="humanIcon" /> SIGN IN</Link>;
+                    <button className="home" onClick={this.handleDemoSubmit}>DEMO</button>;
                 </>
             )
         }else{
