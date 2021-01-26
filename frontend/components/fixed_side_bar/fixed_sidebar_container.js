42e5a9a847ee7fd70b7b97bf1611a7db29524aa8
prabhkirank12 <prabhkirank2@gmail.com>
2021-01-25 20:34:47 -0500

    Removed fixed_side_bar container

--- frontend/components/fixed_side_bar/fixed_sidebar_container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import FixedSideBar from './fixed_sidebar';
-
-export const mapStateToProps = state => ({
-    currentUser: state.entities.users[state.session.id]
-})
-
-export default connect(mapStateToProps, null)(FixedSideBar);
